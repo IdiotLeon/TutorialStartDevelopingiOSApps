f4c9da43e0969d5c6ad0d91344a72cde055be19e
Yang Lu <Leon@leonpro.guest.corp.microsoft.com>
2019-06-20 17:55:16 -0700

    start: Tutorial Food Tracker

---
+++ TutorialFoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorialFoodTracker
+//
+//  Created by Yang Lu on 2019-06-20.
+//  Copyright © 2019 IdiotLeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
